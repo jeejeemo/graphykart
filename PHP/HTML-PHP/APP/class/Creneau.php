baaed30cee4f919a0f60553f105f94af107fb48f
Jean-Mo <jeanmo11@orange.fr>
2020-01-15 13:48:42 +0100

    finChap24

--- PHP/HTML-PHP/APP/class/Creneau.php
+++ PHP/HTML-PHP/APP/class/Creneau.php
@@ -9,8 +9,20 @@ class Creneau{
         $this -> fin = $fin;
        
     }
-    public function inclusHeure(int $heure): bool {
+    public function toHTML(): string
+    {
+        return "<strong>{$this ->debut}h</strong> à <strong>{$this ->fin}h</strong>";
+    }
+    public function inclusHeure(int $heure): bool 
+    {
         return $heure >= $this -> debut && $heure <= $this -> fin;
+    }
+    
+    public function intersect(Creneau $creneau): bool
+    {
+        return $this -> inclusHeure($creneau -> debut) ||
+         $this -> inclusHeure($creneau -> fin)  ||
+         ($this -> debut > $creneau -> debut && $this ->fin < $creneau->fin); 
     }
  
 }
--- PHP/HTML-PHP/APP/test.php
+++ PHP/HTML-PHP/APP/test.php
@@ -6,9 +6,10 @@ $creneau = new Creneau(9,12);
 // $creneau->fin=12;
 $creneau2 = new Creneau(14, 16);
 
-
+echo $creneau -> toHTML();
 
 var_dump(
-    $creneau -> inclusHeure(10),
-    $creneau2 -> inclusHeure(10)
+    // $creneau -> inclusHeure(10),
+    // $creneau2 -> inclusHeure(10)
+    $creneau ->intersect($creneau2)
 );
