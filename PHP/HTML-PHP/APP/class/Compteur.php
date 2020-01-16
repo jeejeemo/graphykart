4b33149ca80190aca41cb1b3a118519620960fa5
Jean-Mo <jmbitoun@gmail.com>
2020-01-16 15:59:30 +0100

    Class Compteur

--- PHP/HTML-PHP/APP/class/Compteur.php
+++ PHP/HTML-PHP/APP/class/Compteur.php
@@ -2,5 +2,27 @@
 
 
 class Compteur {
+	public $fichier;
 
+	public function __construct(string $fichier)
+	{
+		$this -> fichier = $fichier;
+	}
+
+	public function incrementer()
+	{
+		$compteur = 1;
+		if ( file_exists( $this ->fichier ) ) {
+			//si fichier existe on incrémente
+			$compteur = (int) file_get_contents( $this ->fichier );
+			$compteur ++;
+		}
+		file_put_contents($this->fichier,$compteur);
+	}
+	public function recuperer():int{
+		if(!file_exists($this->fichier)){
+			return 0;
+		}
+		return file_get_contents($this->fichier);
+	}
 }
