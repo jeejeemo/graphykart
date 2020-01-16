cf54d100b60f2d502d51a230ffcadd223c26e32d
Jean-Mo <jmbitoun@gmail.com>
2020-01-16 15:56:17 +0100

    creation class Compteur

--- PHP/HTML-PHP/APP/elements/footer.php
+++ PHP/HTML-PHP/APP/elements/footer.php
@@ -3,16 +3,22 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+<footer>
+	<div class = "row">
 
-<div class = "row">
 	<div class="col-md-4">
 
         <?php
-        require_once dirname(__DIR__) .DIRECTORY_SEPARATOR. 'fonctions' .DIRECTORY_SEPARATOR. 'compteur.php';
-        ajouter_vue();
-        $vues = nombre_vues();
+        require_once dirname(__DIR__) .DIRECTORY_SEPARATOR. 'functions.php';
+        require_once dirname(__DIR__) .DIRECTORY_SEPARATOR. 'class' .DIRECTORY_SEPARATOR. 'Compteur.php';
+        $compteur = new Compteur(dirname(__DIR__) .DIRECTORY_SEPARATOR. 'data' .DIRECTORY_SEPARATOR. 'compteur');
+        $compteur -> incrementer();
+        $vues = $compteur ->recuperer();
+
+//        ajouter_vue();
+//        $vues = nombre_vues();
         ?>
-        Il y a <?= nombre_vues()?> visite<?php if($vues>1):?>s <?php endif; ?> sur le site
+        Il y a <?= $vues?> visite<?php if($vues>1):?>s <?php endif; ?> sur le site
 
     </div>
 	<div class="col-md-4"></div>
@@ -26,4 +32,5 @@
 
 </div>
 
-</body>
+</footer>
+
